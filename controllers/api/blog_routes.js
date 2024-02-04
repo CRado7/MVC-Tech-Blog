4ddd30332ee8efcc749558462c4c56f5572b39a8
Christopher <christopher.ferraro34@gmail.com>
2024-02-04 14:47:30 -0500

    Update blog_routes.js

--- controllers/api/blog_routes.js
+++ controllers/api/blog_routes.js
@@ -1,6 +1,64 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { User, Blog, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            attributes: ['id', 'title', 'post', 'date_created'],
+            order: [['date_created', 'DESC']],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                    include: {
+                        model: Comment,
+                        attributes: ['comment', 'date_created'],
+                        include: {
+                            model: User,
+                            attributes: ['username'],
+                        },
+                    },
+                },
+            ],
+        });
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id, {
+            attributes: ['id', 'title', 'post', 'date_created'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                    include: {
+                        model: Comment,
+                        attributes: ['comment', 'date_created'],
+                        include: {
+                            model: User,
+                            attributes: ['username'],
+                        },
+                    },
+                },
+            ],
+        });
+
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog found with this id!' });
+            return;
+        }
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.post('/', withAuth, async (req, res) => {
     try {
@@ -12,6 +70,26 @@ router.post('/', withAuth, async (req, res) => {
         res.status(200).json(newBlog);
     } catch (err) {
         res.status(400).json(err);
+    }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.update(req.body, {
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!blogData[0]) {
+            res.status(404).json({ message: 'No blog found with this id!' });
+            return;
+        }
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
