051ed475fe64a629fcf7edc4e46f84f3dc9c18c7
Christopher <christopher.ferraro34@gmail.com>
2024-02-04 12:00:15 -0500

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,14 @@
+const User = require('./user');
+const Comment = require('./comment');
+const Blog = require('./blog');
+
+User.hasMany(Blog);
+Blog.belongsTo(User);
+
+Blog.hasMany(Comment);
+Comment.belongsTo(Blog);
+
+User.hasMany(Comment);
+Comment.belongsTo(User);
+
+module.exports = { User, Blog, Comment};
