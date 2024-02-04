06886f4edaca5b008cfd7595ff84808907c36cc6
Christopher <christopher.ferraro34@gmail.com>
2024-02-04 14:24:42 -0500

    Update comment_routes.js

--- controllers/api/comment_routes.js
+++ controllers/api/comment_routes.js
@@ -2,6 +2,16 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comment.findAll();
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.post('/', withAuth, async (req, res) => {
     try {
         const newComment = await Comment.create({
@@ -25,7 +35,7 @@ router.delete('/:id', withAuth, async (req, res) => {
         });
 
         if (!commentData) {
-            res.status(404).json({ message: 'No blog found with this id!' });
+            res.status(404).json({ message: 'No comment found with this id!' });
             return;
         }
 
