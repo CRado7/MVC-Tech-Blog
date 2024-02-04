f19130a0047a7ab43a2307ad02749a3af8155173
Christopher <christopher.ferraro34@gmail.com>
2024-02-04 13:36:58 -0500

    Update blog_routes.js

--- controllers/api/blog_routes.js
+++ controllers/api/blog_routes.js
@@ -1,129 +1,38 @@
 const router = require('express').Router();
 const { User, Blog, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
-    Blog.findAll({
-        attributes: ['id', 'title', 'post', 'date'],
-        include: [
-            {
-                model: User,
-                attributes: ['username']
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newBlog = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newBlog);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
             },
-            {
-                model: Comment,
-                attributes: ['id', 'comment', 'date'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            }
-        ]
-    })
-        .then(dbBlogData => res.json(dbBlogData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
         });
-});
 
-router.get('/:id', (req, res) => {
-    Blog.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: ['id', 'title', 'post', 'date'],
-        include: [
-            {
-                model: User,
-                attributes: ['username']
-            },
-            {
-                model: Comment,
-                attributes: ['id', 'comment', 'date'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            }
-        ]
-    })
-        .then(dbBlogData => {
-            if (!dbBlogData) {
-                res.status(404).json({ message: 'No blog found with this id' });
-                return;
-            }
-            res.json(dbBlogData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-router.post('/', (req, res) => {
-    if (!req.session.loggedIn) {
-        res.status(403).json({ message: 'You are not authorized to create a blog. Please Login.' });
-        return;
-    }
-
-    Blog.create({
-        title: req.body.title,
-        post: req.body.post,
-        user_id: req.session.user_id
-    })
-        .then(dbBlogData => res.json(dbBlogData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-router.put('/:id', (req, res) => {
-    if (!req.session.loggedIn) {
-        res.status(403).json({ message: 'You are not authorized to update this blog. Please Login.' });
-        return;
-    }
-
-    Blog.update(req.body, {
-        where: {
-            id: req.params.id
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog found with this id!' });
+            return;
         }
-    })
-        .then(dbBlogData => {
-            if (!dbBlogData) {
-                res.status(404).json({ message: 'No blog found with this id. Please Login.' });
-                return;
-            }
-            res.json(dbBlogData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
 
-router.delete('/:id', (req, res) => {
-    if (!req.session.loggedIn) {
-        res.status(403).json({ message: 'You are not authorized to delete this blog' });
-        return;
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
     }
-
-    Blog.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-        .then(dbBlogData => {
-            if (!dbBlogData) {
-                res.status(404).json({ message: 'No blog found with this id' });
-                return;
-            }
-            res.json(dbBlogData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
 });
 
 module.exports = router;
